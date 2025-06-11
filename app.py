87f77d40f1b31447fdb1715760aaf663ae298a0c
amine khelifi <mohamedamine.khelifi.1@esprit.tn>
2025-06-10 20:38:35 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from connexion.resolver import RestyResolver
 import connexion
 import os
 
-app = connexion.App(__name__, specification_dir='swagger/')
+# Initialize Connexion app
+app = connexion.App(__name__, specification_dir='swagger')
+app.add_api('modelserving_app.yaml', resolver=RestyResolver('api'))
 
 if __name__ == '__main__':
-  app.add_api('modelserving_app.yaml', resolver=RestyResolver('api'))
-  app.run(port=9090)
-
+    app.run(port=9090)
